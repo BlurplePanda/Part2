466d9cd953f527da23cdda4776324c3d9015dc88
BlurplePanda <bootham@wgc.school.nz>
2024-04-24 10:53:32 +1200

    implemented kosaraju's (i think)

--- Components.java
+++ Components.java
@@ -1,9 +1,4 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
 
 //=============================================================================
 //   Finding Components
@@ -19,10 +14,57 @@ public class Components{
 
     
     public static Map<Stop,Integer> findComponents(Graph graph) {
+        Map<Stop, Integer> components = new HashMap<>();
+        for (Stop stop : graph.getStops()) {
+            components.put(stop, -1);
+        }
+        int componentNum = 0;
+        List<Stop> nodeList = new ArrayList<>();
+        Set<Stop> visited = new HashSet<>();
 
+        for (Stop stop : graph.getStops()) {
+            if (!visited.contains(stop)) {
+                forwardVisit(stop, nodeList, visited);
+            }
+        }
+
+        Collections.reverse(nodeList);
+        for (Stop stop : nodeList) {
+            if (components.get(stop) == -1) {
+                backwardVisit(stop, componentNum, components);
+                componentNum++;
+            }
+        }
 
 
         return new HashMap<Stop,Integer>(); //just to make it compile.
+    }
+
+    /**
+     * Search forward from node, putting node on nodeList after visiting everything it can get to.
+     * @param stop the stop (node) to start from
+     * @param nodeList the list to add to after visiting
+     * @param visited the stops that have been visited already
+     */
+    private static void forwardVisit(Stop stop, List<Stop> nodeList, Set<Stop> visited) {
+        if (!visited.contains(stop)) {
+            visited.add(stop);
+            for (Edge edge : stop.getOutEdges()) {
+                Stop neighbour = edge.toStop();
+                forwardVisit(neighbour, nodeList, visited);
+            }
+            nodeList.add(stop);
+        }
+    }
+
+    private static void backwardVisit(Stop stop, int componentNum, Map<Stop, Integer> components) {
+        if (components.get(stop) == -1) {
+            components.put(stop, componentNum);
+            for (Edge edge : stop.getInEdges()) {
+                Stop backNeighbour = edge.fromStop();
+                backwardVisit(backNeighbour, componentNum, components);
+            }
+        }
     }
 
 
